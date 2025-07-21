93cca689cd3a0ab3ba90c18efa2a5eaaf7a8b82f
kaliassistant <1325077@gmail.com>
2025-07-21 21:14:43 +0800

    fix name prefix plbm

--- package/oh-my-bash/oh-my-bash.mk
+++ package/oh-my-bash/oh-my-bash.mk
@@ -1,9 +1,9 @@
-OHMYBASH_VERSION = font-foxjack
-OHMYBASH_SITE = https://github.com/KaliAssistant/oh-my-bash.git
-OHMYBASH_SITE_METHOD = git
-OHMYBASH_INSTALL_TARGET = YES
+OH_MY_BASH_VERSION = font-foxjack
+OH_MY_BASH_SITE = https://github.com/KaliAssistant/oh-my-bash.git
+OH_MY_BASH_SITE_METHOD = git
+OH_MY_BASH_INSTALL_TARGET = YES
 
-define OHMYBASH_INSTALL_TARGET_CMDS
+define OH_MY_BASH_INSTALL_TARGET_CMDS
 	# Install to /root/.oh-my-bash
 	mkdir -p $(TARGET_DIR)/root/.oh-my-bash
 	cp -r $(@D)/* $(TARGET_DIR)/root/.oh-my-bash/
@@ -11,6 +11,7 @@ define OHMYBASH_INSTALL_TARGET_CMDS
 	# Install default .bashrc from template
 	cp $(TARGET_DIR)/root/.oh-my-bash/templates/bashrc.osh-template \
 	   $(TARGET_DIR)/root/.bashrc
+	echo '[ -f ~/.bashrc ] && . ~/.bashrc' > $(TARGET_DIR)/root/.profile
 endef
 
 $(eval $(generic-package))
